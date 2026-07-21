1f31a21e0ac513866f6f2dff80a7e4411c4091ff
Desoxyno <laurentiedgar@gmail.com>
2026-07-21 17:09:41 +0200

    Systeme de camera fini, peut avoir un nombre illimité de camera, code indépendant

--- src/game/systems/camera/securitycamera.h
+++ src/game/systems/camera/securitycamera.h
@@ -9,4 +9,12 @@ class SecurityCamera
 public:
     std::string name;
     Camera camera;
+    SecurityCamera()
+    {
+        this->camera.position = (Vector3) {0.0f, 0.0f, 0.0f};
+        this->camera.target = (Vector3) {0.0f, 0.0f, 0.0f};
+        this->camera.up = (Vector3) {0.0f, 1.0f, 0.0f};
+        this->camera.fovy = 45.0f;
+        this->camera.projection = CAMERA_PERSPECTIVE;
+    };
 };
